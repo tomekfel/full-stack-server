95d3cd1a8406f70831c256a204b90cec65f547c1
Tomek Felisiak <tomekfel@googlemail.com>
2018-06-18 19:39:48 +0100

    :sparkles: Add cors!

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const createError = require('http-errors');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.json({
